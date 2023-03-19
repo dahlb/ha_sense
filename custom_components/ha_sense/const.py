8046a449d7ac94b1fec0bc13bd2fe9b55f6124b5
Brendan Dahl <dahl.brendan@gmail.com>
2023-03-18 23:35:17 -0400

    fix: release #3

--- custom_components/ha_sense/const.py
+++ custom_components/ha_sense/const.py
@@ -5,5 +5,4 @@ LOGGER: Logger = getLogger(__package__)
 
 NAME = "Integration ha_sense"
 DOMAIN = "ha_sense"
-VERSION = "0.0.0"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
+ATTRIBUTION = "Data provided by dahlb"
